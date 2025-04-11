40b05bf8b8ecfdf1340dd4624c5037438e6ff4f3
GiacomF <giacomofiumana@gmial.com>
2025-04-11 16:33:07 +0200

    Added the EditorWindow
    Saves can't be changed inside the window, so thinking about replacing the List with a ScriptableObj. This way, closin the Window or the engine won't lose the settings

---
+++ Building Tool/Assets/Scripts/Tool/BuildingTool.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildingTool : EditorWindow
+{
+    [MenuItem("Tools/BuildingTool")]
+    public static void ShowWindow()
+    {
+        GetWindow<BuildingTool>("Build Tool");
+    }
+
+    public List<GameObject> compList = new List<GameObject>();
+
+    private void GenerateComponent(int listPos)
+    {
+        Instantiate(compList[listPos], new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    void HandlePrefabList()
+    {
+
+        SerializedObject serObj = new SerializedObject(this);
+        SerializedProperty listProp = serObj.FindProperty("compList");
+        
+        serObj.Update();
+
+        GUILayout.Label("ComponentsList", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(listProp, true);
+
+        serObj.ApplyModifiedProperties();
+    }
+
+    private void OnGUI()
+    {
+        HandlePrefabList();
+
+        GUILayout.BeginVertical();
+
+        if(GUILayout.Button("Floor"))
+        {
+            GenerateComponent(0);
+        }
+
+        if(GUILayout.Button("Wall"))
+        {
+            GenerateComponent(1);
+        }
+
+        if(GUILayout.Button("Roof"))
+        {
+            GenerateComponent(2);
+        }
+
+        GUILayout.EndVertical();
+
+        this.SaveChanges();
+    }
+}
