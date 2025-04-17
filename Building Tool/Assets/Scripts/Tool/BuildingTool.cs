e7134f60f90281444d4889b4275563a9324a4168
GiacomF <giacomofiumana@gmial.com>
2025-04-17 11:32:56 +0200

    Asset load from Folder
    BUilding Components get added through search in assets folder

--- Building Tool/Assets/Scripts/Tool/BuildingTool.cs
+++ Building Tool/Assets/Scripts/Tool/BuildingTool.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,19 +13,11 @@ public class BuildingTool : EditorWindow
 
     enum COMPONENTTYPE
     {
-        None,
         Floor,
-        Wall,
-        Roof
+        Roof,
+        Wall
     }
 
-    private List<GameObject> components = new List<GameObject>();
-
-    GameObject pointer;
-    private void GenerateComponent(int listPos, Vector3 pos)
-    {
-        Instantiate(settings.compList[listPos], pos, Quaternion.identity);
-    }
 
     Ray ray;
     void OnSceneGUI(SceneView sceneView)
@@ -33,9 +26,9 @@ public class BuildingTool : EditorWindow
         Ray ray = HandleUtility.GUIPointToWorldRay(currEvent.mousePosition);
 
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity) && currPreview != null)
         {
-
+            currPreview.transform.position = hit.point;
         }
     }
 
@@ -45,8 +38,40 @@ public class BuildingTool : EditorWindow
         settings = (Settings_SO)EditorGUILayout.ObjectField("Settings_SO", settings, typeof(Settings_SO), false);
     }
 
+    private List<GameObject> components = new List<GameObject>();
+    void LoadPrefabs(string path)
+    {
+        if(!Directory.Exists(path))
+        {
+            Debug.LogError("Invalid asset path");
+            return;
+        }
+
+        string[] files = Directory.GetFiles(path, "*.prefab", SearchOption.TopDirectoryOnly);
+
+        components.Clear();
+        foreach(string file in files)
+        {
+            components.Add(AssetDatabase.LoadAssetAtPath<GameObject>(file));
+        }
+
+        Debug.Log($"Added {components.Count} components");
+    }
+
+    GameObject currPreview;
+    COMPONENTTYPE typeSelected;
+    void CreatePreview()
+    {
+        if(currPreview != null)
+        {
+            DestroyImmediate(currPreview);
+        }
+
+        currPreview = Instantiate(components[(int)typeSelected]);
+    }
+
     bool isToolOn = false;
-    COMPONENTTYPE typeSelected = COMPONENTTYPE.None;
+    string folderPath;
     private void OnGUI()
     {
         HandleSettings();
@@ -55,18 +80,33 @@ public class BuildingTool : EditorWindow
         {
             SceneView.duringSceneGui += OnSceneGUI;
 
+            EditorGUI.BeginChangeCheck();
+
             GUILayout.BeginVertical();
+
+            GUILayout.Label("Load Asset Path", EditorStyles.boldLabel);
+            folderPath = GUILayout.TextField(folderPath);
+
+            if(GUILayout.Button("Load Prefabs"))
+            {
+                LoadPrefabs(folderPath);
+            }
 
             typeSelected = (COMPONENTTYPE)EditorGUILayout.EnumPopup("Component Selected", typeSelected);
 
             GUILayout.EndVertical();
+
+            if(EditorGUI.EndChangeCheck())
+            {
+                if(components.Count > 0)
+                CreatePreview();
+            }
         }
 
         else
         {
             SceneView.duringSceneGui -= OnSceneGUI;
             isToolOn = false;
-            DestroyImmediate(pointer);
         }
 
         if(GUILayout.Button("Tool On"))
@@ -78,7 +118,5 @@ public class BuildingTool : EditorWindow
     private void OnDisable()
     {    
         SceneView.duringSceneGui -= OnSceneGUI;
-        if(pointer != null)
-        DestroyImmediate(pointer);
     }
 }
